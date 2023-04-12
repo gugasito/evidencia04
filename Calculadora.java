ba47d122b67b939aea61d34060e71d75029f282e
BenjaminCruzado <b.cruzado01@ufromail.cl>
2023-04-12 09:36:51 -0400

    Subo las funciones que se deben realizar para el punto 3

--- Calculadora.java
+++ Calculadora.java
@@ -1,2 +1,56 @@
 public class Calculadora {
+    public static double perimetroCuadrado(){
+        return 0.0;
+    }
+    public static double areaCuadrado(){
+        return 0.0;
+    }
+    public static double volumenCuadrado(){
+        return 0.0;
+    }
+    public static double perimetroRectangulo(){
+        return 0.0;
+    }
+    public static double areaRectangulo(){
+        return 0.0;
+    }
+    public static double volumenRectangulo(){
+        return 0.0;
+    }
+    public static double perimetroCirculo(){
+        return 0.0;
+    }
+    public static double areaCirculo(){
+        return 0.0;
+    }
+    public static double volumenCirculo(){
+        return 0.0;
+    }
+    public static double perimetroEsfera(){
+        return 0.0;
+    }
+    public static double areaEsfera(){
+        return 0.0;
+    }
+    public static double volumenEsfera(){
+        return 0.0;
+    }
+    public static double perimetroCubo(){
+        return 0.0;
+    }
+    public static double areaCubo(){
+        return 0.0;
+    }
+    public static double volumenCubo(){
+        return 0.0;
+    }
+    public static double perimetroCono(){
+        return 0.0;
+    }
+    public static double areaCono(){
+        return 0.0;
+    }
+    public static double volumenCono(){
+        return 0.0;
+    }
 }
